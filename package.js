2424c15fba7b135ac7b37255e98b02be91d23423
Roshan Jobanputra <rosh93@gmail.com>
2014-09-08 11:59:03 -0700

    Update panel positioning to stay on right of screen on large res displays.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Dynamic slide panel for meteor. Replacement for modals on responsive sites.",
-  version: "0.0.3",
+  version: "0.0.4",
   git: "https://github.com/rosh93/meteor-slide-panel"
 });
 
@@ -13,7 +13,7 @@ Package.onUse(function(api) {
 
   api.use('iron:dynamic-template@0.4.1');
 
-  api.versionsFrom('METEOR@0.9.1');
+  api.versionsFrom('METEOR@0.9.1.1');
   api.addFiles(['slidepanel.js', 'slidepanel_template.html', 'slidepanel_template.js', 'slidepanel.css'], ['client']);
   api.export(['slidePanel'], ['client']);
 });
